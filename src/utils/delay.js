c9507ec382c1b908a86e4a8b49067e0ac63f0665
何志刚 <zhiganghe1201@outlook.com>
2020-08-28 21:54:57 +0800

    优化delay

--- src/utils/delay.js
+++ src/utils/delay.js
@@ -3,10 +3,33 @@
  * 添加一个阻塞  ms毫秒后再执行；
  * @param {*} ms 
  */
-function delay(ms) {
+async function delay(ms = 1000) {
 	return new Promise(reslove => {
 		setTimeout(() => {
-			reslove()
+			reslove(ms)
 		}, ms);
 	})
 }
+
+
+delay(2000).then(res => {
+	console.log(res); // 2000 
+})
+
+
+
+/**
+ * 回调模式
+ */
+
+ function delay1(duration, callback) {
+	 setTimeout(() => {
+		 callback(null, duration)
+	 }, duration);
+ }
+
+ function cb(err, d) {
+	 console.log(d);
+ }
+
+ delay1(10, cb);
